4e6e8047c4830fbdd5224cb90282f9f3c74d4fa0
Mathieu Bordier-Aupy <mathieu.bordieraupy@gmail.com>
2024-04-13 15:03:51 +0200

    Removed export tag
    Removed export tag to avoid a breaking error.
    Error: `GD0102`
    [Documentation](https://docs.godotengine.org/en/4.2/tutorials/scripting/c_sharp/diagnostics/GD0102.html)

--- Scripts/SettingsResource.cs
+++ Scripts/SettingsResource.cs
@@ -14,7 +14,7 @@ public partial class SettingsResource : Resource
 	[Export] public string ModsLocation;
 	[Export] public int InstalledVersion = -1;
 	[Export] public bool LightModeEnabled;
-	[Export] public Mode AppMode;
+	public Mode AppMode; // Why is this in export? I removed it for now, else the program can't be compiled.
 	[Export] public bool Muted = true;
 	[Export] public string AppDataPath;
 	[Export] public string GithubApiToken;
